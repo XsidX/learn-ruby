ab702dc85ae98561a430702f893c0bfb7a8dc664
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 15:40:04 +0300

    introduction to object methods

---
+++ getting-started/intro-to-object-methods.rb
@@ -0,0 +1,15 @@
+p "hello world".length
+p "hello world".upcase
+p "hello world".downcase
+
+foo = "sidney"
+
+p foo.length
+
+p 10.next
+p -1.next
+
+puts
+
+puts "Hello world"
+puts "Hello world".inspect
