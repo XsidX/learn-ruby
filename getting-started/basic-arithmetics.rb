af5c65a012000b23fc4b9ab5b09721ca4162d0ba
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 14:54:25 +0300

    basic arithmetics

---
+++ getting-started/basic-arithmetics.rb
@@ -0,0 +1,21 @@
+p 1 + 4
+
+p 4 - 3
+
+p 3 * 4
+
+p "School " + "bus"
+
+p 10 / 5
+
+# ruby always wants to return an integer result back even when u have decimals
+p 12 / 5
+
+# add a floating point number to correct the above
+p 12.0 / 5
+
+# exponents
+p 2 ** 2
+
+# modulus
+p 5 % 2
