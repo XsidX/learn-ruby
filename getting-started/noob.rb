0b02d58f5c0a247454b4c6f05969fbf010bdffad
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 14:26:23 +0300

    puts method

---
+++ getting-started/noob.rb
@@ -0,0 +1,13 @@
+puts "Hello World"
+puts "I'm alive and well"
+
+puts "I made $65 dollars or a profit of 25%!"
+
+puts 5
+
+puts 3.14159 * 2
+puts
+puts
+puts
+puts "puts always creates a line break  even if it does not output something to the screen"
+puts "3" + "4"
