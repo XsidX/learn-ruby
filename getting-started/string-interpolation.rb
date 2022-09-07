8c2155e65e40e6800b0c37250357487abac55eb3
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 16:00:45 +0300

    string interpolation

---
+++ getting-started/string-interpolation.rb
@@ -0,0 +1,14 @@
+p 5
+# convert number to string
+p 5.to_s
+
+NAME = "Sidney"
+age = 25
+
+# string interpolation
+# automatically converts the data to make it fit the string
+p " my name is #{NAME}, how are you?. I am #{age} years old"
+puts
+p "i am " + age.to_s + " years old"
+puts
+p "adding 1 + 1 is #{1 + 1}"
