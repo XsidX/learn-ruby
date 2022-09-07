8016208bc22a77d29943269d96a99fa0282e85ef
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 14:36:26 +0300

    p method

---
+++ getting-started/p.rb
@@ -0,0 +1,12 @@
+puts "Steven Segal"
+# p offers additional clarity on how things operate
+p "Steven Segal"
+
+puts "Hi there, this is a
+big line break"
+puts
+p "Hi there, this is a
+big line break"
+puts
+p "5"
+p 5
