47780634750962fbb177bd9a48c44a70ce563620
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 15:51:44 +0300

    return values and nil object

---
+++ getting-started/return-values.rb
@@ -0,0 +1,20 @@
+# irb(main):002:0> "hello"
+# => "hello"
+# irb(main):003:0> 4+5
+# => 9
+# irb(main):004:0> "hello world" + "!"
+# => "hello world!"
+# irb(main):005:0> puts 5 + 3
+# 8
+# => nil                                           
+# irb(main):006:0> puts nil
+
+# => nil                                           
+# irb(main):007:0> p 4+4
+# 8
+# => 8                                             
+# irb(main):008:0> p nil
+# nil
+# => nil                                           
+# irb(main):009:0> 
+                       
