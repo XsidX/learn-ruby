776e5bd3b0d6dcda22ded0652cb4585b668682d9
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 15:27:09 +0300

    constants

---
+++ getting-started/constants.rb
@@ -0,0 +1,18 @@
+age = 25
+p age
+
+age = 30
+p age
+
+NAME = "Sid"
+PI = 3.14159
+
+p NAME
+p PI
+
+# string interpolation
+p "My name is #{NAME}"
+
+# this is a no no, multiple warnings!! returned
+NAME = 'NotSid'
+p NAME
