369da7f0d8ad27ab2e7389a78a6d4aa9a5871fcc
Sidney Kaguli <developer.sid@outlook.com>
2022-09-07 15:10:21 +0300

    variables

---
+++ getting-started/variables.rb
@@ -0,0 +1,11 @@
+# a varable is a placeholder/identifier/refrence/pointer to an oblect
+
+name = "Sidney"
+last_name = "Kaguli"
+handsome = true
+age = 24
+
+
+puts "My name is " + name + " " + last_name + ". I am"
+
+
