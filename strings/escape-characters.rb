e8ab7b1655d82eb3c4772148cd516267455c61a8
Sidney Kaguli <developer.sid@outlook.com>
2022-09-08 17:03:04 +0300

    escape characters

---
+++ strings/escape-characters.rb
@@ -0,0 +1,10 @@
+puts "Hello"
+puts 'Hello'
+
+puts "Sid said 'goodbye' to Tiffah"
+puts 'Sid said "goodbye" to Tiffah'
+
+puts "Sid said \"goodbye\" to Tiffah"
+
+puts "Let's add a line break\nright heare"
+puts "\tInsert a tab right here"
