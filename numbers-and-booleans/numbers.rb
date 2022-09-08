18c62bdb27dc3318a28e052cf1d247dbbc067db0
Sidney Kaguli <developer.sid@outlook.com>
2022-09-08 13:11:58 +0300

    intro to numbers

---
+++ numbers-and-booleans/numbers.rb
@@ -0,0 +1,16 @@
+# intergers from the integer class
+p 6
+p 0
+p 1
+
+# floating point number
+p 2.345
+p 4.32323
+
+# all number are also objects
+
+p 2.next
+p 4.to_s
+
+# .class shows us the class from which the object was made
+p 4.class
