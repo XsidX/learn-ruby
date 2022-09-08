a58767609c8d07230fc85ab938cee41605a5986a
Sidney Kaguli <developer.sid@outlook.com>
2022-09-08 13:26:22 +0300

    number, string, float conversions

---
+++ numbers-and-booleans/convert-numbers.rb
@@ -0,0 +1,19 @@
+str = "5"
+
+p str
+p str.class
+# convert to integer
+p str.to_i.class
+# original str variable not mutated
+p str
+# convert to a float
+p str.to_f
+puts
+num = 10
+p num.class
+# convert to str
+p num.to_s.class
+puts
+pi = 3.14
+p pi.to_i
+p pi.to_s
