9927ad534a7cb1a47b87983045c6935489ee1bb5
Sidney Kaguli <developer.sid@outlook.com>
2022-09-22 12:44:15 +0300

    self keyword

--- classes-I/Classes/gadget.rb
+++ classes-I/Classes/gadget.rb
@@ -5,15 +5,17 @@ class Gadget
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 
-  # overriding the to_s method available in the object class above
+  # self refers to the object at hand whenever it is defined inside an instance method.
+  # outside an instance method definition, it refers to the class
   def to_s
-    "Gadget #{@production_number} has the username #{@username}"
+    "Gadget #{@production_number} has the username #{@username}. It is made from the #{self.class} classs and it has the ID #{self.object_id}"
   end
 end
 
 phone = Gadget.new
-# info is an instance method that is only defined in the confines of an object
-# it is an instance method
-
+laptop = Gadget.new
 
 puts phone.to_s
+puts
+puts
+puts laptop.to_s
