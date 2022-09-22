77233b8d52db008b677a364cfcb6dbf4fc5431c0
Sidney Kaguli <developer.sid@outlook.com>
2022-09-22 12:32:12 +0300

    instance methods

--- classes-I/Classes/gadget.rb
+++ classes-I/Classes/gadget.rb
@@ -4,6 +4,17 @@ class Gadget
     @password = "topsecret"
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
+
+
+  def info
+    "Gadget #{@production_number} has the username #{@username}"
+  end
 end
 
 phone = Gadget.new
+laptop = Gadget.new
+
+# info is an instance method that is only defined in the confines of an object
+# it is an instance method
+puts phone.info
+puts laptop.info
