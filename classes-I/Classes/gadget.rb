c35a5a41a4b98330f0252a3deebc0808b79d1498
Sidney Kaguli <developer.sid@outlook.com>
2022-09-22 12:37:23 +0300

    overriding the to_s method

--- classes-I/Classes/gadget.rb
+++ classes-I/Classes/gadget.rb
@@ -5,16 +5,15 @@ class Gadget
     @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
   end
 
-
-  def info
+  # overriding the to_s method available in the object class above
+  def to_s
     "Gadget #{@production_number} has the username #{@username}"
   end
 end
 
 phone = Gadget.new
-laptop = Gadget.new
-
 # info is an instance method that is only defined in the confines of an object
 # it is an instance method
-puts phone.info
-puts laptop.info
+
+
+puts phone.to_s
