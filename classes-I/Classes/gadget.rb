874ee7828341792b5dff755cb451a27b20d8a83a
Sidney Kaguli <developer.sid@outlook.com>
2022-09-22 12:24:07 +0300

    instance variables and the initialize method

--- classes-I/Classes/gadget.rb
+++ classes-I/Classes/gadget.rb
@@ -1,8 +1,9 @@
 class Gadget
+  def initialize
+    @username = "User #{rand(1..100)}"
+    @password = "topsecret"
+    @production_number = "#{("a".."z").to_a.sample}-#{rand(1..999)}"
+  end
 end
 
 phone = Gadget.new
-laptop = Gadget.new
-microwave = Gadget.new
-
-puts phone
