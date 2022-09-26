c343966f3e1add1cc3eaca9160f8c57a09dc3652
Sidney Kaguli <developer.sid@outlook.com>
2022-09-26 16:15:31 +0300

    using super keyword

--- Classes-III/subclasses.rb
+++ Classes-III/subclasses.rb
@@ -15,29 +15,36 @@ class Employee
 end
 
 class Manager < Employee
+
+  attr_reader :rank
+
+  def initialize(name, age, rank)
+    super(name, age)
+    @rank = rank
+  end
+
   def yell
     puts "Who's the boss? I'm the boss!"
   end
+
   def introduce
-    puts "Hi, my name is #{name} and I am a manager. Woohoo!"
+    result = super
+    result += " I'm also a manager."
   end
+
 end
 
-class Worker < Employee
-  def clock_in(time)
-    puts "Starting my shift at #{time}"
-  end
-
-  def yell
-    puts "I'm working! I'm working!"
-  end
-end
-
-bob = Worker.new("Bob", 52)
-bob.introduce
-bob.yell
-
-
-sally = Manager.new("Sally", 26)
+sally = Manager.new("Sally", 42, "Senior Vice President")
 sally.introduce
+sally.rank
+sally.name
+sally.age
+
+
+# rick = Employee.new("Rick", 34)
+# rick.introduce
+# rick.rank
+# rick.name
+  
+
 
